04471de12dfeeb5135b086196d4764894fa2ee6e
Pickledtezcat <>
2021-05-25 03:01:55 +0000

    finished challenge

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,9 +2,21 @@ import numpy as np
 
 #starting project
 
-def calculate(list):
+def calculate(number_list):
 
+  calc = {}
 
+  if len(number_list) < 9:
+    raise ValueError("List must contain nine numbers.")  
 
-
-    return false
+  a = np.array(number_list)
+  b = a.reshape(3,3)
+  
+  calc["mean"] = [b.mean(axis=0).tolist(), b.mean(axis=1).tolist(), a.mean()]
+  calc["variance"] = [b.var(axis=0).tolist(), b.var(axis=1).tolist(), a.var()]
+  calc["standard deviation"] = [b.std(axis=0).tolist(), b.std(axis=1).tolist(), a.std()]
+  calc["max"] = [b.max(axis=0).tolist(), b.max(axis=1).tolist(), a.max().tolist()]
+  calc["min"] = [b.min(axis=0).tolist(), b.min(axis=1).tolist(), a.min()]
+  calc["sum"] = [b.sum(axis=0).tolist(), b.sum(axis=1).tolist(), a.sum()]
+  
+  return calc
